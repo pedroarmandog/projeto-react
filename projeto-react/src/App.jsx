d73c7055471c879702705d40a33204326fbf5422
Pedro Armando <pedroarmandodev@gmail.com>
2026-01-29 10:45:07 -0300

    aprendendo react

---
+++ projeto-react/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  const name = 'Pedro Armando'
+  return (
+    <>
+      <h1>Olá React!</h1>
+      <p>Meu primeiro projeto em React</p>
+      <p>Meu nome é {name}</p>
+    </>
+  )
+}
+
+export default App
